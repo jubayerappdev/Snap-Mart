50153d3709913fc64f1362f6fbedd074a0551984
jubo445 <juboraj335.ja@gmail.com>
2025-10-21 21:01:35 +0600

    runtime permission added

---
+++ app/src/main/java/com/creativeitinstitute/snapmart/data/models/Product.kt
@@ -0,0 +1,12 @@
+package com.creativeitinstitute.snapmart.data.models
+
+data class Product (
+    var name: String = "",
+    var price: Double = 0.0,
+    var imageLink: String = "",
+    var description: String = "",
+    var amount:Int = 0,
+    var sellerID: String = "",
+    var productID: String = ""
+
+)
--- app/src/main/java/com/creativeitinstitute/snapmart/views/dashboard/seller/upload/UploadProductFragment.kt
+++ app/src/main/java/com/creativeitinstitute/snapmart/views/dashboard/seller/upload/UploadProductFragment.kt
@@ -1,22 +1,95 @@
 package com.creativeitinstitute.snapmart.views.dashboard.seller.upload
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
 import androidx.fragment.app.Fragment
 import com.creativeitinstitute.snapmart.R
+import com.creativeitinstitute.snapmart.base.BaseFragment
+import com.creativeitinstitute.snapmart.core.areAllPermissionGranted
+import com.creativeitinstitute.snapmart.core.extract
+import com.creativeitinstitute.snapmart.core.requestPermission
+import com.creativeitinstitute.snapmart.data.models.Product
+import com.creativeitinstitute.snapmart.databinding.FragmentUploadProductBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class UploadProductFragment : Fragment() {
+@AndroidEntryPoint
+class UploadProductFragment : BaseFragment<FragmentUploadProductBinding>(
+    FragmentUploadProductBinding::inflate
+) {
+    private lateinit var permissionsRequest: ActivityResultLauncher<Array<String>>
+    lateinit var product: Product
+    override fun setListener() {
 
+        permissionsRequest = getPermissionRequest()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_product, container, false)
+        binding.apply {
+            ivProduct.setOnClickListener {
+                requestPermission(permissionsRequest, permissionList)
+            }
+
+            btnUploadProduct.setOnClickListener {
+
+                val name = etProductName.extract()
+                val price = etProductPrice.extract()
+                val description = etProductDescription.extract()
+                val amount = etProductAmount.extract()
+
+                product = Product(
+                    name = name,
+                    description = description,
+                    price = price.toDouble(),
+                    amount = amount.toInt()
+                )
+
+                uploadProduct(product)
+            }
+        }
+
     }
+
+    private fun getPermissionRequest(): ActivityResultLauncher<Array<String>> {
+
+        return registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+
+            if (areAllPermissionGranted(permissionList)){
+
+                //ase
+                Toast.makeText(requireContext(),"Ase", Toast.LENGTH_LONG).show()
+
+            }else{
+                //nai
+                Toast.makeText(requireContext(),"Nai", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
+    }
+
+    private fun uploadProduct(product: Product) {
+
+
+    }
+
+    override fun allObserver() {
+
+    }
+
+    companion object{
+        private val permissionList = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        )
+    }
+
+
 
 
 }
